97061dc9587f0fb33a7c8d534687ec64fdceba73
Gasofa06 <rogerrovi2006@gmail.com>
2024-03-19 17:25:17 +0000

    PHP and HTML.

--- apps/web/layouts/main/home.php
+++ apps/web/layouts/main/home.php
@@ -1,32 +1,35 @@
 <?php
-include($_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/notice.php");
+include($_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/notice.html");
 ?>
 
 <!DOCTYPE html>
 
 <html lang="en" theme="light">
 
-<?php
-include($_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/head.html");
+<?php 
+include( $_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/head.html");
 ?>
 
 <body data-barba="wrapper">
-  <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous">
+    </script>
 
-  <script src="https://cdn.jsdelivr.net/npm/@barba/core@2.9.7/dist/barba.umd.min.js" integrity="sha256-JhRJFoR0Bcz8x0FUX+R7/qd8OEn+CL4BWcZzxzfkEgs=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@barba/core@2.9.7/dist/barba.umd.min.js"
+        integrity="sha256-JhRJFoR0Bcz8x0FUX+R7/qd8OEn+CL4BWcZzxzfkEgs=" crossorigin="anonymous"></script>
 
-  <?php
-  include($_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/loading.php");
-  include($_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/navigation.php");
-  ?>
+    <?php
+      include($_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/loading.php");
+      include($_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/navigation.php");
+    ?>
 
-  <main data-barba="container" data-barba-namespace="home">
-    <section></section>
-  </main>
+    <main data-barba="container" data-b arba-namespace="home">
+        <section></section>
+    </main>
 
-  <?php
-  include($_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/footer.php");
-  ?>
+    <?php
+      include($_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/footer.php");
+    ?>
 </body>
 
 </html>
