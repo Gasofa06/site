9f7154cf9d64594c97aac2b294457d2293b4dd3f
Gasofa06 <rogerrovi2006@gmail.com>
2024-03-20 22:55:47 +0000

    CSS and PHP.

--- apps/web/layouts/main/home.php
+++ apps/web/layouts/main/home.php
@@ -1,5 +1,5 @@
 <?php
-include($_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/notice.html");
+  include($_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/notice.html");
 ?>
 
 <!DOCTYPE html>
@@ -7,29 +7,17 @@ include($_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/notice.html");
 <html lang="en" theme="light">
 
 <?php 
-include( $_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/head.html");
+    include( $_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/head.html");
 ?>
 
 <body data-barba="wrapper">
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
-        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous">
-    </script>
-
-    <script src="https://cdn.jsdelivr.net/npm/@barba/core@2.9.7/dist/barba.umd.min.js"
-        integrity="sha256-JhRJFoR0Bcz8x0FUX+R7/qd8OEn+CL4BWcZzxzfkEgs=" crossorigin="anonymous"></script>
-
     <?php
-      include($_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/loading.php");
-      include($_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/navigation.php");
+      include($_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/navigation.html");
     ?>
 
     <main data-barba="container" data-b arba-namespace="home">
         <section></section>
     </main>
-
-    <?php
-      include($_SERVER['DOCUMENT_ROOT'] . "/layouts/reuse/footer.php");
-    ?>
 </body>
 
 </html>
